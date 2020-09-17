e8581f9fa3ca235d2feb18f6c2b65acfa1cb1149
Lenin Ochoa <isleor@hotmail.com>
2020-09-17 10:13:34 +0200

    Patient Domain Model eingefügt.

---
+++ SheClean/SheClean.Domain/Models/Patient.cs
@@ -0,0 +1,11 @@
+﻿namespace SheClean.Domain.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Nachname { get; set; }
+        public string PatientNr { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
