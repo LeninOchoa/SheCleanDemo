373c18de02a44c46b74675a61974400f1875bc5c
Lenin Ochoa <isleor@hotmail.com>
2020-09-17 15:24:39 +0200

    Domain.Core eingefügt und implementation von InMemoryBus

---
+++ SheClean/SheClean.Domain.Core/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SheClean.Domain.Core.Events;
+
+namespace SheClean.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public  DateTime Timestamp { get; protected set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
--- SheClean/SheClean.Infra.Bus/InMemoryBus.cs
+++ SheClean/SheClean.Infra.Bus/InMemoryBus.cs
@@ -1,7 +1,22 @@
-﻿namespace SheClean.Infra.Bus
+﻿using System.Threading.Tasks;
+using MediatR;
+using SheClean.Domain.Core.Bus;
+using SheClean.Domain.Core.Commands;
+
+namespace SheClean.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
-        
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
